4bae052c7de7ac516089823ac0748a3a6c8a32d3
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-13 15:40:42 +0530

    create sample student data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 function App() {
   const [count, setCount] = useState(0)
 
+  const sampleStudentData = [
+    {
+      name: "Hansi Piyimika",
+      date: "16-09-2024",
+      reg: "FSDC2401"
+    },
+    {
+      name: "Isuri Tharuka",
+      date: "16-09-2024",
+      reg: "FSDC2402"
+    },
+    {
+      name: "Athadudaralalage Tharuka Nadun Bandara",
+      date: "16-09-2024",
+      reg: "FSDC2403"
+    }
+  ]
+
   return (
     <>
-      
+  
     </>
   )
 }
